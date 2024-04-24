29c4dd9c5f29d2f98f0aa9c7031106b12c47dafd
MahinourAbdelgawad <159120250+MahinourAbdelgawad@users.noreply.github.com>
2024-04-24 22:08:26 +0200

    created graph class and edited main

--- Graph.h
+++ Graph.h
@@ -1,11 +1,16 @@
 #pragma once
+#include <string>
+#include <unordered_map>
+using namespace std;
 
 class Graph
 {
 
+	unordered_map<string, vector<string>> adjList;
+
 public:
 
-	Graph();
+	Graph() {} //constructor
 
 	//change return type and parameters according to what u need
 	void AddCity();
--- main.cpp
+++ main.cpp
@@ -1,6 +1,48 @@
 #include "Graph.h"
+#include <iostream>
+using namespace std;
+
 
 int main()
 {
+	int choice;
 
+	//call function to load and read file
+	//create graph from data in the file
+
+	cout << "--------------MINI WASALNY APPLICATION--------------" << endl << endl;
+
+	cout << "Please select an option:" << endl
+		<< "(1) Display available cities" << endl //displaygraph
+		<< "(2) Enter a city" << endl
+		<< "(3) Remove a city" << endl
+		<< "(4) Navigate between two cities" << endl
+		<< "(5)                 " << endl //update
+		<< "(6) Exit Program" << endl << endl;
+
+	cin >> choice;
+
+	//exception handling
+
+	switch (choice)
+	{
+		case(1): //display
+			break;
+		case(2): //addcity
+			break;
+		case(3): //removecity
+			break;
+		case(4): //findshortest
+			break;
+		case(5): //update
+			break;
+		default: //exit
+		{
+			//save data into file
+			exit(0);
+		}
+	}
+
+	return main();
+		
 }
