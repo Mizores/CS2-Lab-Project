2275d1de02c4d7317946acfe750545aaa33a23fe
MahinourAbdelgawad <159120250+MahinourAbdelgawad@users.noreply.github.com>
2024-04-24 14:44:05 +0200

    created files

---
+++ Graph.h
@@ -0,0 +1,17 @@
+#pragma once
+
+class Graph
+{
+
+public:
+
+	Graph();
+
+	//change return type and parameters according to what u need
+	void AddCity();
+	void DeleteCity();
+	void FindShortestDis();
+	void UpdateGraph();
+
+
+};
