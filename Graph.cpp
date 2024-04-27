e4955a13136ed0cb2829b238d9e757feff251554
Mizores <49450729+Mizores@users.noreply.github.com>
2024-04-27 03:02:47 +0300

    Update Graph.cpp

--- Graph.cpp
+++ Graph.cpp
@@ -23,3 +23,12 @@ void Graph::DeleteCity(string city)
 		edges = newEdges; //replacing old edges vector with the new one
 	}
 }
+
+ void Graph::AddCity(const string& src_or_city, const string& dest = "", float distance = 0.0) {
+        if (dest == "") {
+            adjList.insert({src_or_city, vector<pair<string, float>>()});
+        } else {
+            adjList[src_or_city].push_back({dest, distance});
+            adjList[dest].push_back({src_or_city, distance});
+        }
+    }
